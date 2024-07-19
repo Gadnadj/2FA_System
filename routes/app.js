e57a9af94dd93e65c6663d911c53fc8fa000e850
Gad Nadjar <gadnadjar@gmail.com>
2024-07-19 10:42:42 +0300

    update

--- routes/app.js
+++ routes/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require('../routes/auth');
+const authRoutes = require('./auth');
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/2fa_project', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MongoDB connected successfully');
+    })
+    .catch(err => {
+        console.error('MongoDB connection error:', err);
+    });
 
 app.use('/auth', authRoutes);
 
